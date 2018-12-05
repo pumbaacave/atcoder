8a3f28440d4c48ba5692521d600d0a3b681b520f
若帆 叶 <ruofan.ye@gu3.co.jp>
2018-12-05 17:05:27 +0900

    add day4 part2

--- adventOfCode/day4.py
+++ adventOfCode/day4.py
@@ -51,6 +51,7 @@ def findSleepyGuard(summary):
     print(c)
     print(int(a) * (int(c[0]))) # +1 is not needed, because
 
+
 def run():
     print('start')
     with open('input4', 'r') as f:
@@ -60,4 +61,51 @@ def run():
     summay = sumSleepTime(sorted(data))
     findSleepyGuard(summay)
 
-run()
+
+def sumSleepFreq(data):
+    summary = {}
+    gen = (record for record in data)
+    ID = ''  # initialisation
+    try:
+        while(True):
+            record = next(gen)
+            if (record[4] == 'Guard'):
+                # str
+                ID = id_matcher.match( record[5]).group('id')
+                if ID not in summary:
+                    summary[ID] = 0
+            else:
+                sleep = record[3]
+                wake = next(gen)[3]
+                summary[ID] += (wake - sleep)
+
+            continue
+
+    except Exception as E:
+        print("summary end")
+    return summary
+
+
+def findFrequentSleepGuard(summary):
+    sort_s = sorted(summary.items(), key=lambda x: max(x[1]), reverse=True)
+    a = sort_s[0][0]
+    b = max(sort_s[0][1])
+    print(a)
+    print(b)
+    c = numpy.where(sort_s[0][1] == b)
+    print(c)
+    print(int(a) * (int(c[0]))) # +1 is not needed, because
+
+def run1():
+    print('start')
+    with open('input4', 'r') as f:
+        data = [parse(line.strip()) for line in f]
+
+    #  pprint.pprint(sorted(data))
+    summay = sumSleepTime(sorted(data))
+    pprint.pprint(summay)
+    findFrequentSleepGuard(summay)
+
+
+run1()
+

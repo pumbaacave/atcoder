1f71128886ffa1551bb88948d529725cdc1babd1
若帆 叶 <ruofan.ye@gu3.co.jp>
2018-12-10 18:49:54 +0900

    [add] advent of code Day8 part 1, test driven, wow

---
+++ adventOfCode/day8.py
@@ -0,0 +1,56 @@
+def run():
+    in_list = read_input()
+    # print(in_list)
+    print(parse_tree(in_list))
+
+def read_input():
+    total_list = []
+    with open('input8', 'r') as f:
+        for line in f:
+            num_list = line.split(' ')
+            total_list.extend(map(int, num_list))
+    return total_list
+
+def sum_meta(in_list):
+    tree_length, meta_sum = parse_tree(in_list)
+    return meta_sum
+
+def parse_tree(in_list):
+    node_num = in_list[0]
+    meta_num = in_list[1]
+    # print("number of node:{}".format(node_num))
+    # print("number of meta:{}".format(meta_num))
+    if node_num == 0:
+        return parse_single_node(in_list)
+
+    children_node_length = 0
+    total_meta_sum = 0
+    idx = 2
+    for _ in range(node_num):
+        # print("tree starts at: {}".format(idx))
+        tree_length, child_meta_sum = parse_tree(in_list[idx:])
+        idx += tree_length
+        children_node_length += tree_length
+        total_meta_sum  += child_meta_sum
+    # print(children_node_length)
+    # print(child_meta_sum)
+    for i in range(meta_num):
+        meta_temp = in_list[2 + i + children_node_length]
+        # print("find meta: {}, at {}".format(meta_temp, 2 + i + children_node_length))
+        total_meta_sum += meta_temp
+
+    return children_node_length + 2 + meta_num, total_meta_sum
+
+
+def parse_single_node(in_list):
+    node_num = in_list[0]
+    meta_num = in_list[1]
+    assert node_num == 0
+    meta_sum = 0
+    for i in range(meta_num):
+        meta_temp = in_list[2 + i]
+        # print("find meta: {}, at {}".format(meta_temp, 1 + i))
+        meta_sum += meta_temp
+    return 2 + meta_num, meta_sum
+
+run()
---
+++ adventOfCode/test_day8.py
@@ -0,0 +1,4 @@
+import day8
+def test_sum_meta():
+    IN = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+    assert day8.sum_meta(IN) == 138

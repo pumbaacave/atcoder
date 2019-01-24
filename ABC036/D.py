94c2e5459f6515e5b86bc08d8f1a8e9b5149da84
若帆 叶 <ruofan.ye@gu3.co.jp>
2019-01-24 11:18:07 +0900

    [add] add black white bridge

---
+++ ABC036/D.py
@@ -0,0 +1,82 @@
+"""
+https://abc036.contest.atcoder.jp/tasks/abc036_d#
+問題文
+N 個の島があります。 島には 1 から N までの番号がついています。 また、N−1 個の橋があります。 i 番目の橋は ai 番の島と bi 番の島をつないでいます。 どの島からどの島へも橋をいくつか経由して到達できることがわかっています。 
+
+すぬけ君は、それぞれの島を白または黒に塗ることにしました。 ただし、両端の島が黒で塗られているような橋があってはいけません。 色の塗り方が何通りあるか、109+7 で割った余りを求めてください。
+制約
+2≤N≤105
+1≤ai,bi≤N
+どの島からどの島へも橋をいくつか経由して到達できる
+
+"""
+
+from loguru import logger
+from itertools import chain
+from functools import reduce
+from collections import Counter
+class WBTree(object):
+    class Node(object):
+        def __init__(self, children=[]):
+            self.children = children
+
+        def white(self):
+            if not self.children:
+                return 2
+            else:
+                return reduce(lambda x, y: x * y , [c.white() for c in self.children])
+        def black(self):
+            if not self.children:
+                return 1
+            else:
+                return reduce(lambda x, y: x + y , [c.black() for c in self.children])
+
+    def __init__(self, bridge_list):
+        pass
+
+def cal(N, bridges):
+    bridge_cnt = Counter()
+    bw_list = [[0,0] for _ in range(N+1)]
+    con_list = [[] for _ in range(N+1)]
+    for l, r in bridges:
+        bridge_cnt[r] += 1
+        bridge_cnt[l] += 1
+        con_list[l].append(r)
+        con_list[r].append(l)
+
+    logger.debug(bridge_cnt)
+    sorted_c = sorted(bridge_cnt, key=lambda key: bridge_cnt[key]) # cnt min first
+    logger.debug(sorted_c)
+
+    max_iland = sorted_c[-1]
+    sentinel = object()
+    ite = chain((key for key in sorted_c), [sentinel])
+
+    for nxt in ite:
+
+        if nxt is sentinel:
+            break
+
+        if bridge_cnt[nxt] == 1:
+            bw_list[nxt][0] = 1 #cal black value
+            bw_list[nxt][1] = 1 #cal white value
+        else:
+            # parent cnt > child cnt
+            # so parent bw_value is [0,0]
+            logger.debug(nxt)
+            bw_list[nxt][0] = reduce(lambda x,y: x*y, [bw_list[con][1] for con in con_list[nxt] if bw_list[con][0] > 0])
+            bw_list[nxt][1] = reduce(lambda x,y: x*y, [bw_list[con][1] + bw_list[con][0] for con in con_list[nxt] if bw_list[con][1] > 0])
+
+    logger.debug(bw_list)
+    return sum(bw_list[max_iland])
+
+
+def test_sample1():
+    N = 5
+    bridges = [
+            [2, 5],
+            [1, 5],
+            [2, 4],
+            [3, 2]
+            ]
+    assert  14 == cal(N, bridges)

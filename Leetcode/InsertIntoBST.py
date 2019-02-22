5a198d47af91c799aec21515882ecc8816f9031d
若帆 叶 <ruofan.ye@gu3.co.jp>
2019-02-22 09:14:47 +0900

    add BSTInsert

---
+++ Leetcode/InsertIntoBST.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def insertIntoBST(self, root: 'TreeNode', val: 'int') -> 'TreeNode':
+        if not root:
+            return TreeNode(val)
+
+        if val < root.val:
+            if not root.left:
+                root.left = TreeNode(val)
+            else:
+                self.insertIntoBST(root.left, val)
+        else:
+            if not root.right:
+                root.right= TreeNode(val)
+            else:
+                self.insertIntoBST(root.right, val)
+
+        return root
+        

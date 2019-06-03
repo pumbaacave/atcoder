ddc36ce9769a18d21fcc99afed65e70523bd1951
叶若帆 <ruofan.ye@gu3.co.jp>
2019-06-03 10:06:23 +0900

    optimize MinWindow

--- Leetcode/Hard/MinWindow.py
+++ Leetcode/Hard/MinWindow.py
@@ -9,19 +9,26 @@ class Solution:
         retl, retr = 0, 0
         l = 0
         ret = 2 << 10
+        def update(l, r):
+            nonlocal retl, retr, ret
+            win_len = r - l + 1
+            if win_len < ret:
+                ret = win_len
+                retl, retr = l, r
+
         for r, ch in enumerate(s):
             seen[ch] += 1
-            while l<=r and (seen[s[l]] > t[s[l]] or s[l] not in t):
-                if s[l] not in t:
+            # fulfill
+            if not bool(t - seen):
+                update(l, r)
+                # shrink
+                while l<=r and (seen[s[l]] > t[s[l]] or s[l] not in t):
+                    seen[s[l]] -= 1
                     l += 1
+                    update(l, r)
                 else:
                     seen[s[l]] -= 1
                     l += 1
-            if len((t - seen).keys()) == 0:
-                win_len = r - l + 1
-                if win_len < ret:
-                    ret = win_len
-                    retl, retr = l, r
 
         if not bool(t - Counter(s[retl:retr+1])):
             return s[retl:retr+1]
